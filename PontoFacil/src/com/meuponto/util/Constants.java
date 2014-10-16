4c00600fc1ee6d8109985f1cb3d7cae937cdd2f7
Uilton Santos <uilton.jose@gmail.com>
2014-10-16 11:24:46 -0300

    Refactory da classe Constants e FontManager

--- PontoFacil/src/com/meuponto/util/Constants.java
+++ PontoFacil/src/com/meuponto/util/Constants.java
@@ -3,6 +3,38 @@ package com.meuponto.util;
 public class Constants {
 
 	public static enum RobotoFontType {
-		ROBOTO_BLACK, ROBOTO_BLACK_ITALIC, ROBOTO_BOLD, ROBOTO_BOLD_ITALIC, ROBOTO_ITALIC, ROBOTO_LIGHT, ROBOTO_LIGHT_ITALIC, ROBOTO_MEDIUM, ROBOTO_MEDIUM_ITALIC, ROBOTO_REGULAR, ROBOTO_THIN, ROBOTO_THIN_ITALIC, ROBOTO_CONDENSED_BOLD, ROBOTO_CONDENSED_BOLD_ITALIC, ROBOTO_CONDENSED_ITALIC, ROBOTO_CONDENSED_LIGHT, ROBOTO_CONDENSED_LIGTH_ITALIC, ROBOTO_CONDENSED_REGULAR;
+		ROBOTO_BLACK("fonts/Roboto-Black.ttf"), 
+		ROBOTO_BLACK_ITALIC("fonts/Roboto-BlackItalic.ttf"), 
+		ROBOTO_BOLD("fonts/Roboto-Bold.ttf"), 
+		ROBOTO_BOLD_ITALIC("fonts/Roboto-BoldItalick.ttf"),
+		ROBOTO_ITALIC("fonts/Roboto-Italic.ttf"), 
+		ROBOTO_LIGHT("fonts/Roboto-Light.ttf"), 
+		ROBOTO_LIGHT_ITALIC("fonts/Roboto-LightItalic.ttf.ttf"), 
+		ROBOTO_MEDIUM("fonts/Roboto-Medium.ttf"), 
+		ROBOTO_MEDIUM_ITALIC("fonts/Roboto-MediumItalic.ttf"), 
+		ROBOTO_REGULAR("fonts/Roboto-Regular.ttf"), 
+		ROBOTO_THIN("fonts/Roboto-Thin.ttf"), 
+		ROBOTO_THIN_ITALIC("fonts/Roboto-ThinItalic.ttf"), 
+		ROBOTO_CONDENSED_BOLD("fonts/RobotoCondensed-Bold.ttf"), 
+		ROBOTO_CONDENSED_BOLD_ITALIC("fonts/RobotoCondensed-BoldItalic.ttf"), 
+		ROBOTO_CONDENSED_ITALIC("fonts/RobotoCondensed-Italic.ttf"), 
+		ROBOTO_CONDENSED_LIGHT("fonts/RobotoCondensed-Light.ttf"), 
+		ROBOTO_CONDENSED_LIGTH_ITALIC("fonts/RobotoCondensed-LightItalic.ttf"), 
+		ROBOTO_CONDENSED_REGULAR("fonts/RobotoCondensed-Regular.ttf");
+
+		private String fontTypeFace;
+
+		private RobotoFontType(String fontTypeFace) {
+			this.setFontTypeFace(fontTypeFace);
+		}
+
+		public String getFontTypeFace() {
+			return fontTypeFace;
+		}
+
+		public void setFontTypeFace(String fontTypeFace) {
+			this.fontTypeFace = fontTypeFace;
+		}
 	}
+
 }
--- PontoFacil/src/com/meuponto/util/FontManager.java
+++ PontoFacil/src/com/meuponto/util/FontManager.java
@@ -9,73 +9,8 @@ public class FontManager {
 
 	private static Typeface fontTypeFace;
 
-	public static Typeface getFontTeste(AssetManager manager) {
-		if (fontTypeFace == null) {
-			fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-Black.ttf");
-		}
-		return fontTypeFace;
-	}
-
 	public static Typeface getRobotoFont(RobotoFontType type, AssetManager manager) {
-		switch (type) {
-			case ROBOTO_BLACK:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-Black.ttf");
-				break;
-			case ROBOTO_BLACK_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-BlackItalic.ttf");
-				break;
-			case ROBOTO_BOLD:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-Bold.ttf");
-				break;
-			case ROBOTO_BOLD_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-BoldItalick.ttf");
-				break;
-			case ROBOTO_CONDENSED_BOLD:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/RobotoCondensed-Bold.ttf");
-				break;
-			case ROBOTO_CONDENSED_BOLD_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/RobotoCondensed-BoldItalic.ttf");
-				break;
-			case ROBOTO_CONDENSED_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/RobotoCondensed-Italic.ttf");
-				break;
-			case ROBOTO_CONDENSED_LIGHT:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/RobotoCondensed-Light.ttf");
-				break;
-			case ROBOTO_CONDENSED_LIGTH_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/RobotoCondensed-LightItalic.ttf");
-				break;
-			case ROBOTO_CONDENSED_REGULAR:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/RobotoCondensed-Regular.ttf");
-				break;
-			case ROBOTO_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-Italic.ttf");
-				break;
-			case ROBOTO_LIGHT:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-Light.ttf");
-				break;
-			case ROBOTO_LIGHT_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-LightItalic.ttf.ttf");
-				break;
-			case ROBOTO_MEDIUM:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-Medium.ttf");
-				break;
-			case ROBOTO_MEDIUM_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-MediumItalic.ttf");
-				break;
-			case ROBOTO_REGULAR:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-Regular.ttf");
-				break;
-			case ROBOTO_THIN:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-Thin.ttf");
-				break;
-			case ROBOTO_THIN_ITALIC:
-				fontTypeFace = Typeface.createFromAsset(manager, "fonts/Roboto-ThinItalic.ttf");
-				break;
-			default:
-				break;
-		}
-
+		fontTypeFace = Typeface.createFromAsset(manager, type.getFontTypeFace());
 		return fontTypeFace;
 	}
 }
