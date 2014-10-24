676bf0fe740cc33abfbcb95a182f7bdac7450dec
Diogo M. Lins <diogo.moreira@gmail.com>
2014-10-24 18:19:00 -0300

    To Clock In Fragment

---
+++ PontoFacil/src/com/meuponto/ui/fragments/ToClockInFragment.java
@@ -0,0 +1,22 @@
+package com.meuponto.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ToClockInFragment extends Fragment {
+
+	public ToClockInFragment() {
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+
+}
